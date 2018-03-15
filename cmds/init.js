e264beca9800240c3d388227d9e890def2573dda
Mathew <mathew.corm@gmail.com>
2018-03-15 14:49:03 -0400

    configLoader

--- cmds/init.js
+++ cmds/init.js
@@ -1,4 +1,5 @@
 const opencollab = require('opencollab-lib')
+const configLoader = require('../lib/config-loader')
 
 // TODO: Initialization options
 module.exports = function(program) {
@@ -8,8 +9,12 @@ module.exports = function(program) {
 		.version('0.0.0')
 		.description('Initialize new OpenCollab repo')
 		.action(async (name, description, options) => {
+			const config = configLoader()
+
 			try {
-				let result = await opencollab.init(process.cwd(), { name, description })
+				console.log(config)
+				//console.log(options.config.key('account'))
+				//let result = await opencollab.init(process.cwd(), { name, description })
 			} catch(e) {
 				console.log('Error: ', e)
 			}
